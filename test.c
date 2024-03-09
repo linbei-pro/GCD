c5ac154c39ff36d88a88a8c878dd62535a48862b
linbei-pro <148841405+linbei-pro@users.noreply.github.com>
2024-03-09 13:05:20 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int gcd(int m, int n)
+{
+	if (n == 0)
+	{
+		return m;
+	}
+	return gcd(n, m % n);
+}
+
+int main() {
+
+	int a = 0;
+	int b = 0;
+	scanf_s("%d %d", &a, &b);
+
+	printf("%d",gcd(a, b));
+
+
+	return 0;
+}
